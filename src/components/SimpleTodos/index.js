93b4ad24d12da5c04e9a3e9a7e2a5fcadb50ea83
Charan Krishna <charankrishnaa310@gmail.com>
2025-04-26 11:21:51 +0000

    enhancement-simpletodo

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -2,65 +2,133 @@ import {Component} from 'react'
 import './index.css'
 import TodoItem from '../TodoItem'
 
-const initialTodosList = [
-  {
-    id: 1,
-    title: 'Book the ticket for today evening',
-  },
-  {
-    id: 2,
-    title: 'Rent the movie for tomorrow movie night',
-  },
-  {
-    id: 3,
-    title: 'Confirm the slot for the yoga session tomorrow morning',
-  },
-  {
-    id: 4,
-    title: 'Drop the parcel at Bloomingdale',
-  },
-  {
-    id: 5,
-    title: 'Order fruits on Big Basket',
-  },
-  {
-    id: 6,
-    title: 'Fix the production issue',
-  },
-  {
-    id: 7,
-    title: 'Confirm my slot for Saturday Night',
-  },
-  {
-    id: 8,
-    title: 'Get essentials for Sunday car wash',
-  },
-]
+// const initialTodosList = [
+//   {
+//     id: 1,
+//     title: 'Book the ticket for today evening',
+//     status: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Rent the movie for tomorrow movie night',
+//     status: false,
+//   },
+//   {
+//     id: 3,
+//     title: 'Confirm the slot for the yoga session tomorrow morning',
+//     status: false,
+//   },
+//   {
+//     id: 4,
+//     title: 'Drop the parcel at Bloomingdale',
+//     status: false,
+//   },
+//   {
+//     id: 5,
+//     title: 'Order fruits on Big Basket',
+//     status: false,
+//   },
+//   {
+//     id: 6,
+//     title: 'Fix the production issue',
+//     status: false,
+//   },
+//   {
+//     id: 7,
+//     title: 'Confirm my slot for Saturday Night',
+//     status: false,
+//   },
+//   {
+//     id: 8,
+//     title: 'Get essentials for Sunday car wash',
+//     status: false,
+//   },
+// ]
 
 // Write your code here
 
 class SimpleTodos extends Component {
-  state = {Todolist: initialTodosList}
+  state = {todolist: JSON.parse(localStorage.getItem('todolist')), input: ''}
 
   deleteTask = id => {
-    const {Todolist} = this.state
-    const filteredTask = Todolist.filter(each => each.id !== id)
+    const {todolist} = this.state
+    const filteredTask = todolist.filter(each => each.id !== id)
     console.log(filteredTask)
-    this.setState({Todolist: filteredTask})
+    this.setState({todolist: filteredTask})
+  }
+
+  additem = () => {
+    const {input, todolist} = this.state
+    if (input !== '') {
+      const newtodo = {
+        id: todolist.length + 1,
+        title: input,
+      }
+      this.setState(prev => ({
+        todolist: [...prev.todolist, newtodo],
+        input: '',
+      }))
+    }
+  }
+
+  editItem = (id, editvale) => {
+    const {todolist} = this.state
+    if (editvale !== '') {
+      const updatedlist = todolist.map(each => {
+        if (each.id === id) {
+          return {...each, title: editvale}
+        }
+        return each
+      })
+      this.setState({todolist: updatedlist})
+    }
+  }
+
+  taskCompleat = id => {
+    const {todolist} = this.state
+    const updatedlist = todolist.map(each => {
+      if (each.id === id) {
+        return {...each, status: !each.status}
+      }
+      return each
+    })
+    this.setState({todolist: updatedlist})
+  }
+
+  saveChanges = () => {
+    const {todolist} = this.state
+    localStorage.setItem('todolist', JSON.stringify(todolist))
   }
 
   render() {
-    const {Todolist} = this.state
+    const {todolist, input} = this.state
     return (
       <div className="container">
         <div className="inner">
           <h1 className="heading">Simple Todos</h1>
+          <input
+            type="text"
+            placeholder="enter todo"
+            onChange={event => {
+              this.setState({input: event.target.value})
+            }}
+            className="input"
+            value={input}
+          />
+          <button type="button" className="add-btn" onClick={this.additem}>
+            ADD
+          </button>
+          <button type="button" className="add-btn" onClick={this.saveChanges}>
+            Save all
+          </button>
           <ul className="list">
-            {Todolist.map(each => (
+            {todolist.map(each => (
               <TodoItem
                 item={each}
                 deleteTask={this.deleteTask}
                 key={each.id}
+                editItem={this.editItem}
+                taskCompleat={this.taskCompleat}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,21 +1,55 @@
 // Write your code here
+import {useState} from 'react'
 import './index.css'
 
 const TodoItem = props => {
-  const {item, deleteTask} = props
+  const {item, deleteTask, editItem, taskCompleat} = props
   const {id, title} = item
+  const [isEdit, setEdit] = useState(false)
+  const [editvale, setEditvale] = useState(item.title)
+  const tackclass = item.status ? 'task complet' : 'task'
 
   const onDelete = () => {
     deleteTask(id)
   }
 
+  const onEdit = () => {
+    setEdit(prev => !prev)
+    editItem(id, editvale)
+  }
+
+  const onTaskcompleted = () => {
+    taskCompleat(id)
+  }
+
   return (
     <li className="li">
       <div className="li-item">
-        <p className="task">{title}</p>
-        <button className="button" type="button" onClick={onDelete}>
-          Delete
-        </button>
+        <div className="check-para">
+          <input
+            type="checkbox"
+            className="checkbox"
+            onChange={onTaskcompleted}
+          />
+          {isEdit ? (
+            <input
+              type="text"
+              className="edit-input"
+              value={editvale}
+              onChange={event => setEditvale(event.target.value)}
+            />
+          ) : (
+            <p className={tackclass}>{title}</p>
+          )}
+        </div>
+        <div className="button-div">
+          <button type="button" className="button" onClick={onEdit}>
+            {isEdit ? 'Save' : 'Edit'}
+          </button>
+          <button className="button" type="button" onClick={onDelete}>
+            Delete
+          </button>
+        </div>
       </div>
     </li>
   )
